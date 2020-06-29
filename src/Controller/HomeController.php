2ce56ea1ff9450e8cf4b86016647b886db5d0beb
David De Brito <5621971-DavidDebrito@users.noreply.gitlab.com>
2020-06-29 14:10:39 +0200

    réagencement card page index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -112,9 +112,9 @@ class HomeController extends AbstractController
         $product = $this->getDoctrine()->getRepository(Products::class)->findOneBy(['slug' => $slug]);
 
         // Erreur 404 si aucun article trouvé
-        // if (!$product) {
-        //     throw $this->createNotFoundException('Cet article est inéxistant.');
-        // }
+        if (!$product) {
+            throw $this->createNotFoundException('Cet article est inéxistant.');
+        }
 
         //Ajout du formulaire
         //Instanciation de l'entité Comments
