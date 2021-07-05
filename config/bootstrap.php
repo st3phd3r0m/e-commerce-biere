46f2690f39630d7798679260846b3b457d686018
Stéphane Derom <stephane.derom@gmail.com>
2021-07-05 07:27:06 +0200

    disable dotenv check 2

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -10,12 +10,12 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (is_array($env = @include dirname(__DIR__) . '/.env.local.php') && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
-    (new Dotenv(false))->populate($env);
-} else {
-    // load all the .env files
-    (new Dotenv(false))->loadEnv(dirname(__DIR__) . '/.env');
-}
+// if (is_array($env = @include dirname(__DIR__) . '/.env.local.php') && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
+//     (new Dotenv(false))->populate($env);
+// } else {
+//     // load all the .env files
+//     (new Dotenv(false))->loadEnv(dirname(__DIR__) . '/.env');
+// }
 
 $_SERVER += $_ENV;
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
